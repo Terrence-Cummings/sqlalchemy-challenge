51cfaf1aef1c593f438e82f2adf2cb9d643bc7d6
Terrence Cummings <terrence.cummings@gmail.com>
2020-05-08 08:36:08 -0500

    skeleton app py

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-#Lesson 1
-
-# 1. import Flask
 from flask import Flask
 
-# 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
 
-
-# 3. Define what to do when a user hits the index route
 @app.route("/")
-def home():
+def welcome():
     print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page! help butthead"
+    return (
+        f"Welcome to Surf's Up weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        
+    )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    print("Server received request for 'Precipitation' page...")
+    return "Welcome to the Precipitation page!"
 
-# 4. Define what to do when a user hits the /about route
-# @app.route("/about")
-# def about():
-#     print("Server received request for 'About' page...")
-#     return "Welcome to my 'About' page!"
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'Stations' page...")
+    return "Welcome to the Stations page!"
 
+@app.route("/api/v1.0/tobs")
+def temperatures():
+    print("Server received request for 'Temperatures' page...")
+    return "Welcome to the Temperatures page!"
+
+@app.route("/api/v1.0/<start>")
+def temp_start():
+    print("Server received request for 'Min, Max, Avg Start' page...")
+    return "Welcome to the Min, Max, Avg Start Date page!"
+
+@app.route("/api/v1.0/<start>/<end>")
+def temp_start_end():
+    print("Server received request for 'Min, Max, Avg Start End' page...")
+    return "Welcome to the Min, Max, Avg Start End Date page!"
 
 if __name__ == "__main__":
     app.run(debug=True)
