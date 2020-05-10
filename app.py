2a14c73861d99644c80ee6b12c532a798743e116
Terrence Cummings <terrence.cummings@gmail.com>
2020-05-10 12:03:57 -0500

    app is working

--- app.py
+++ app.py
@@ -65,22 +65,19 @@ def welcome():
     print("Server received request for 'Home' page...")
     return (
         f"Welcome to Surf's Up weather API!<br>"
-        f"<br>"
+        f"We collect precipitation and temperature data from weather stations on the island of Oahu in Hawaii.<br><br>"
         f"Earliest date of data = {earliest_date}<br>"
-        f"Latest date of data = {latest_date}<br>"
-        f"<br>"
-        f"Available Routes:<br>"
-        f"<br>"
+        f"Latest date of data = {latest_date}<br><br>"
+        f"Available URL Routes:<br><br>"
         f"Below URL returns JSON of precipitation on Oahu on each day between {year_ago_latest} and {latest_date}.<br>"
-        f"/api/v1.0/precipitation<br><br>"
+        f"Copy this URL to browser: /api/v1.0/precipitation<br><br>"
         f"Below URL returns JSON of temperature on Oahu on each day between {year_ago_latest} and {latest_date}.<br>"
-        f"/api/v1.0/temperature<br><br>"
+        f"Copy this URL to browser: /api/v1.0/temperature<br><br>"
         f"Below URL returns JSON of the weather stations on Oahu.<br>"
-        f"/api/v1.0/stations<br><br>"
-        f"Below URL returns the max, min, and avg temperature on Oahu between the START and END dates provided by the user in the URL.<br>"
-        f"START and END dates in the URL must be entered in the form YYYY-MM-DD.<br>"
+        f"Copy this URL to browser: /api/v1.0/stations<br><br>"
+        f"Below URL returns the max, min, and avg temperature on Oahu encompassing the START and END dates provided by the user in the URL.<br>"
         f"If no END date provided in the URL then END date is assume to be {latest_date}<br>"
-        f"/api/v1.0/START/END"
+        f"Copy this URL to browser and replace START/END with dates in YYYY-MM-DD format: /api/v1.0/START/END"
     )
 
 
@@ -88,15 +85,18 @@ def welcome():
 def precipitation():
     print("Server received request for 'Precipitation' page...")
     session = Session(engine)
+    #Query precipitation observations for last year
     date_prcp_query = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date<=latest_date_dt).filter(Measurement.date>year_ago_latest_dt)
+        filter(Measurement.date<=latest_date_dt).filter(Measurement.date>=year_ago_latest_dt)
     session.close()
 
+    #Save as df and grab only the max precip observation for each day in the last year
     date_prcp_df = pd.DataFrame(date_prcp_query, columns=['Date', 'Precipitation'])
     date_prcp_df.set_index('Date', inplace=True)
     date_prcp_df.dropna(inplace=True)
     date_prcp_df.sort_index(inplace=True)
     date_prcp_max = date_prcp_df.groupby('Date')[['Precipitation']].max()
+    #Turn into dictionary for jsonification
     prcp_query_dict = date_prcp_max.to_dict()
 
     return jsonify(prcp_query_dict)
@@ -105,32 +105,48 @@ def precipitation():
 def stations():
     print("Server received request for 'Stations' page...")
     session = Session(engine)
-    station_names = session.query(Station.name).all()
+    #Query all the weather station details
+    station_query = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation)
+    station_df = pd.DataFrame(station_query, columns = ['station', 'name', 'latitude', 'longitude', 'elevation'])
+    station_df.set_index('station', inplace=True)
+    station_df.dropna(inplace=True)
     session.close()
-    all_stats = list(np.ravel(station_names))
-    return jsonify(all_stats)
+    #Make a dictionary of weather station characteristics for jsonification
+    station_dict = station_df.to_dict(orient='index')
+
+    return jsonify(station_dict)
 
 @app.route("/api/v1.0/temperature/")
 def temperatures():
     print("Server received request for 'Temperatures' page...")
 
     session = Session(engine)
+    #Query temperature observations for the last year at the station with the most observations
     tobs_date_query = session.query(Measurement.date, Measurement.tobs).\
     filter(Measurement.date<=latest_date_dt).filter(Measurement.date>=year_ago_latest_dt).\
     filter(Measurement.station==max_stat_freq)
     session.close()
-
+    #Save query as df
     tobs_date_df = pd.DataFrame(tobs_date_query, columns=['Date','Temperature'])
     tobs_date_df.set_index('Date', inplace=True)
     tobs_date_df.dropna(inplace=True)
+    #Transform df into dictionary for jsonification
     tobs_date_dict = tobs_date_df.to_dict()
 
     return jsonify(tobs_date_dict)
 
 @app.route("/api/v1.0/<start>/")
 def temp_start(start):
+    #Control on START date within database date range
+    if start<earliest_date or start>latest_date:
+        return (
+            f"START must be between {earliest_date} and {latest_date}.<br>"
+            f"/api/v1.0/START"
+        )
+
     print("Server received request for 'Min, Max, Avg Start End' page...")
     session = Session(engine)
+    #Query max, min, and avg temperature between START date and last date in database
     TMAX = session.query(func.max(Measurement.tobs)).\
         filter(Measurement.date<=latest_date).filter(Measurement.date>=start).all()
     TMIN = session.query(func.min(Measurement.tobs)).\
@@ -138,9 +154,10 @@ def temp_start(start):
     TAVG = session.query(func.avg(Measurement.tobs)).\
         filter(Measurement.date<=latest_date).filter(Measurement.date>=start).all()
     session.close()
-
+    #Round TAVG for presentation
     TAVG = round(TAVG[0][0],1)
 
+    #Calc number of days in the query for information
     days_obs = latest_date_dt - dt.datetime.strptime(start, "%Y-%m-%d")
     days_obs = days_obs.days
 
@@ -152,8 +169,21 @@ def temp_start(start):
 
 @app.route("/api/v1.0/<start>/<end>/")
 def temp_start_end(start, end):
+    #Check START and END dates are within the range of database dates
+    if start<earliest_date or start>latest_date or end<earliest_date or end>latest_date:
+        return (
+            f"START and END must be between {earliest_date} and {latest_date}.<br>"
+            f"/api/v1.0/START/END"
+        )
+    #Allow for START and END interchanged in URL
+    if end<start:
+        start_temp = start
+        start = end
+        end = start_temp
+
     print("Server received request for 'Min, Max, Avg Start End' page...")
     session = Session(engine)
+    #Query max, min, and avg temperature between START date and END date in database
     TMAX = session.query(func.max(Measurement.tobs)).\
         filter(Measurement.date<=end).filter(Measurement.date>=start).all()
     TMIN = session.query(func.min(Measurement.tobs)).\
@@ -161,9 +191,9 @@ def temp_start_end(start, end):
     TAVG = session.query(func.avg(Measurement.tobs)).\
         filter(Measurement.date<=end).filter(Measurement.date>=start).all()
     session.close()
-
+    #Round TAVG for presentation
     TAVG = round(TAVG[0][0],1)
-
+    #Calc number of days in the query for information
     days_obs = dt.datetime.strptime(end, "%Y-%m-%d") - dt.datetime.strptime(start, "%Y-%m-%d")
     days_obs = days_obs.days
 
